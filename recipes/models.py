77999cbd16b92a96a3f1541f7cd66e683dfef4c7
JoannaWilk <wielki.wilq@gmail.com>
2018-11-20 15:06:32 +0100

    add Recipe model along with migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+MEAL_TYPES = (
+    ('B', 'Breakfast'),
+    ('D', 'Dinner'),
+    ('L', 'Lunch'),
+    ('S', 'Snack'),
+    ('DS', 'Dessert'),
+)
+
+
+class Recipe(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=300)
+    meal = models.CharField(choices=MEAL_TYPES, default='Lunch', max_length=20)
+    instruction = models.TextField()
+    wege = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
