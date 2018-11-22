b0f7887c9dc6519d976c016a45f5977184a80a42
JoannaWilk <wielki.wilq@gmail.com>
2018-11-22 14:51:09 +0100

    change recipes views to use DRF's features
    * change status returning in Response to use DRF status library
    * change views to use api_view decorators

--- recipes/views.py
+++ recipes/views.py
@@ -1,52 +1,50 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def recipe_list(request):
     """
-    List all recipes, or create a new one
+    List all recipes or create a new one.
     """
     if request.method == 'GET':
         recipes = Recipe.objects.all()
         serializer = RecipeSerializer(recipes, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = RecipeSerializer(data=data)
+        serializer = RecipeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def recipe_detail(request, pk):
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk):
     """
     Retrieve, update or delete a recipe.
     """
     try:
         recipe = Recipe.objects.get(pk=pk)
     except Recipe.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = RecipeSerializer(recipe)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = RecipeSerializer(recipe, data=data)
+        serializer = RecipeSerializer(recipe, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         recipe.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
