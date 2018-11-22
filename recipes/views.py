1b7f4827d416d47de9cbb89445f15295952da824
JoannaWilk <wielki.wilq@gmail.com>
2018-11-22 13:37:58 +0100

    add views for recipe list and recipe detail
    * add view for displaying list of recipes
    * add view for retrieving, updating and deleting recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from recipes.models import Recipe
+from recipes.serializers import RecipeSerializer
+
+
+@csrf_exempt
+def recipe_list(request):
+    """
+    List all recipes, or create a new one
+    """
+    if request.method == 'GET':
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = RecipeSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def recipe_detail(request, pk):
+    """
+    Retrieve, update or delete a recipe.
+    """
+    try:
+        recipe = Recipe.objects.get(pk=pk)
+    except Recipe.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = RecipeSerializer(recipe)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = RecipeSerializer(recipe, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return HttpResponse(status=204)
