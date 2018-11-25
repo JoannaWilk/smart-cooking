76fd4bfe71d21ba981644eea91ea352734e080d8
JoannaWilk <wielki.wilq@gmail.com>
2018-11-25 23:46:19 +0100

    change views to use only generic views

--- recipes/views.py
+++ recipes/views.py
@@ -1,39 +1,18 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 
 
-class RecipeList(mixins.ListModelMixin,
-                 mixins.CreateModelMixin,
-                 generics.GenericAPIView):
+class RecipeList(generics.ListCreateAPIView):
     """List all recipes or create a new one."""
 
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class RecipeDetail(mixins.RetrieveModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   generics.GenericAPIView):
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, update or delete a recipe."""
 
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
