90fd7d49fa7c254e3a6cbbcec0f503e17481e55f
JoannaWilk <wielki.wilq@gmail.com>
2018-11-26 00:45:01 +0100

    change Recipe model
    * add added_by ForeignKey connected with User model
    * add __str__ method

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 MEAL_TYPES = (
     ('B', 'Breakfast'),
     ('D', 'Dinner'),
@@ -15,6 +17,11 @@ class Recipe(models.Model):
     meal = models.CharField(choices=MEAL_TYPES, default='L', max_length=20)
     instruction = models.TextField(blank=True)
     wege = models.BooleanField(blank=True, default=True)
+
+    added_by = models.ForeignKey(User, related_name='recipes', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ('created',)
