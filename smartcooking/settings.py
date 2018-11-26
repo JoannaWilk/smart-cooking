1ea6e962455db869c19907e560a23310916150da
JoannaWilk <wielki.wilq@gmail.com>
2018-11-27 00:36:26 +0100

    add pagination

--- smartcooking/settings.py
+++ smartcooking/settings.py
@@ -119,6 +119,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
